2f4f0c2e83f496737518a72ffca844b1886f4c27
Mike <myke.dmytrenko@gmail.com>
2021-06-14 15:29:15 +0200

    fixed transform

--- src/tf_broadcaster.py
+++ src/tf_broadcaster.py
@@ -12,14 +12,15 @@ import turtlesim.msg
 def handle_pose(msg):
     br = tf2_ros.TransformBroadcaster()
     t = geometry_msgs.msg.TransformStamped()
+    
 
     t.header.stamp = rospy.Time.now()
     t.header.frame_id = "map"
     t.child_frame_id = "camera1"
-    t.transform.translation.x = msg.x
-    t.transform.translation.y = msg.y
-    t.transform.translation.z = -100
-    q = tf_conversions.transformations.quaternion_from_euler(0, 0, msg.theta)
+    t.transform.translation.x = msg.linear.x
+    t.transform.translation.y = msg.linear.y
+    t.transform.translation.z = -50.0
+    q = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
     t.transform.rotation.x = q[0]
     t.transform.rotation.y = q[1]
     t.transform.rotation.z = q[2]
